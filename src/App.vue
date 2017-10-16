71b7726829dedd3ad73202523ae94bb855d0e14b
centsuse <903591127@qq.com>
2017-10-16 22:19:49 +0800

    配置stylus相关依赖

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,43 @@
 <template>
   <div id="app">
-    <div class="header">
-      I am header!
-    </div>
+    <v-header></v-header>
     <div class="tab">
-      I am tab
+      <div class="tab-item">
+        <a link="{path:'/goods'}">
+        商品</a>
+      </div>
+      <div class="tab-item">
+        <a link="{path:'/seller'}">
+        商家</a>
+      </div>
+      <div class="tab-item">
+        <a link="{path:'/ratings'}">
+        评价</a>
+      </div>
     </div>
-    <div class="content">
-      I am content
-    </div>
+    <router-view>
+
+    </router-view>
   </div>
 </template>
 
 <script>
+  import header from './components/header/header.vue';
+  export default {
+    components: {
+      'v-header': header
+    }
+  };
 </script>
 
-<style>
+<style lang="stylus" rel="stylesheet/stylus">
+  #app
+    .tab
+      display: flex
+      width: 100%
+      height: 40px
+      line-height: 40px
+      .tab-item
+        flex: 1
+        text-align: center
 </style>
